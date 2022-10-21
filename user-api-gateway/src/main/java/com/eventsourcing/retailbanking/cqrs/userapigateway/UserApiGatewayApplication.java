b2b3162c6afe56e57b1be56097da3f78d91cebdf
samantrs <https://samantrs@git.rabobank.nl>
2022-10-21 12:00:39 +0200

    api gateway module added

---
+++ user-api-gateway/src/main/java/com/eventsourcing/retailbanking/cqrs/userapigateway/UserApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.eventsourcing.retailbanking.cqrs.userapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiGatewayApplication.class, args);
+	}
+
+}
